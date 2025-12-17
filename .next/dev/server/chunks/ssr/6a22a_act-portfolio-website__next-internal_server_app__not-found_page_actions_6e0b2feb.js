d5bf66422ae0320bf889d0fd3637d9055fe47fe8
sagaralmad2297 <sagaralmad1997@gmail.com>
2025-12-17 10:31:46 +0530

    second commit

---
+++ .next/dev/server/chunks/ssr/6a22a_act-portfolio-website__next-internal_server_app__not-found_page_actions_6e0b2feb.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/react-portfolio-website/.next-internal/server/app/_not-found/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=6a22a_act-portfolio-website__next-internal_server_app__not-found_page_actions_6e0b2feb.js.map
